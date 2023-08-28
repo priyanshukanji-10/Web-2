acdc8900eee4590edda4246072e6525b824c75c5
Priyanshu <priyanshukanji0@gmail.com>
2023-08-28 13:23:03 +0530

    Implimentation of mongodb is required

--- server.js
+++ server.js
@@ -1,42 +1,14 @@
-const express = require('express')
+const express = require("express");
+const bcrypt= require("bcrypt")
 const app = express()
-const bcrypt = require('bcrypt')
-app.use(express.json())
+const secretKey = "Priyanshu@2005"
+const user = [];
 
-const users = []
 
-app.get('/users', (req, res) => {
-  res.json(users)
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + "../Signup/signup.html");
 })
-
-app.post('/users', async (req, res) => {
-  try {
-    const hashedPassword = await bcrypt.hash(req.body.password, 10)
-    const user = { name: req.body.name, password: hashedPassword }
-    users.push(user)
-    res.status(201).send()
-    console.log(hashedPassword)
-  } catch {
-    res.status(500).send()
-  }
+app.post('/signup', async (req, res) => {
+  const { email, password } = req.body;
+  const hashedPassword=await bcrypt.hash(password,10);
 })
-app.post('/users/login', async (req, res) => {
-    const user = users.find(user => user.name === req.body.name)
-    if (user == null) {
-      return res.status(400).send('Cannot find user')
-    }
-    try {
-      if(await bcrypt.compare(req.body.password, user.password)) {
-        res.send('Success')
-      } else {
-        res.send('Not Allowed')
-      }
-    } catch {
-      res.status(500).send()
-    }
-  })
-  app.get('/',(req,res)=>{
-    res.render('index')
-  })
-  
-  app.listen(5000)
